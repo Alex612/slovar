30e23596365e9a3665e61311a8513eef67d798dd
Alexey Zimin <52530750+Alex612@users.noreply.github.com>
2019-07-08 12:07:33 +0300

    Add files via upload

---
+++ Slovar/Combination.java
@@ -0,0 +1,68 @@
+package com.slov;
+import java.util.Scanner;
+import java.util.Map;
+
+public class Combination implements Combined{ 
+	private FileProcessing fp;
+	private static Scanner sc = new Scanner(System.in);
+	private Validating val = new Validation();
+	
+	
+	Combination(FileProcessing fp)
+	{
+		this.fp=fp;
+	}
+	
+	public Map<String,String> getString(String con){ 
+		if("1".equals(con)){
+		return fp.getMapString();}
+		else
+		{return fp.getMapNumber();}
+	}
+	
+	
+	public String removeKey(String key,String con){
+	try{
+		if("1".equals(con) && "0".equals(val.addValidString(key)) && val.addValidKey(key,4)){
+				return fp.removeKey(key);
+		}
+		if("2".equals(con) && "1".equals(val.addValidInteger(key)) && val.addValidKey(key,5)){
+				return  fp.removeKey(key);
+		}	
+		return "No Key";
+	}catch(Exception e){ return "error";}
+	}
+	
+	
+	
+	public String addKey(String key,String value,String con){  
+	try{
+		if("1".equals(con) && "0".equals(val.addValidString(key)) && val.addValidKey(key,4)){
+				return  fp.addKey(key,value);
+		}
+		if("2".equals(con) && "1".equals(val.addValidInteger(key)) && val.addValidKey(key,5)){
+				return  fp.addKey(key,value);
+		}	
+		return "Invalid Key";
+	}catch(Exception e){ return "error";}
+	}
+	
+	
+	
+	public String getKey(String key,String con){ 
+	try{
+		if("1".equals(con) && "0".equals(val.addValidString(key)) && val.addValidKey(key,4)){
+				return  fp.getKey(key);
+		}
+		if("2".equals(con) && "1".equals(val.addValidInteger(key)) && val.addValidKey(key,5)){
+				return  fp.getKey(key);
+		}	
+		return "No Key";
+	}catch(Exception e){ return "error";}
+	}
+	
+	public String iString(){ 
+		String s = sc.next();
+		return  s;
+	}
+}
---
+++ Slovar/Validating.java
@@ -0,0 +1,10 @@
+package com.slov;
+public interface Validating{
+	
+	public String addValidInteger(String key);
+	
+	public String addValidString(String key);
+	
+	public boolean addValidKey(String key,int dl);
+	
+}
